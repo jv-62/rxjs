e90e98d2ae1881ade514cb961d0c131f3c6849fa
Vinita Jain <jayam6297@gmail.com>
2026-05-27 18:11:13 +0530

    Added back button

--- src/app/observable/observable.component.ts
+++ src/app/observable/observable.component.ts
@@ -1,15 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { Subject } from 'rxjs';
+import { filter, takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-observable',
   templateUrl: './observable.component.html',
-  styleUrls: ['./observable.component.scss']
+  styleUrls: ['./observable.component.scss'],
 })
-export class ObservableComponent implements OnInit {
+export class ObservableComponent implements OnInit, OnDestroy {
+  showBackButton = false;
+  private destroy$ = new Subject<void>();
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private location: Location,
+  ) {}
 
   ngOnInit(): void {
+    this.updateBackButton(this.router.url);
+    this.router.events
+      .pipe(
+        filter((event) => event instanceof NavigationEnd),
+        takeUntil(this.destroy$),
+      )
+      .subscribe((event: NavigationEnd) => {
+        this.updateBackButton(event.urlAfterRedirects);
+      });
   }
 
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
+
+  private updateBackButton(url: string): void {
+    this.showBackButton = url !== '/observable' && url !== '/observable/';
+  }
 }
