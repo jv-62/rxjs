16024d5754cd01e2f6af0fe6e478a0b38986ba96
Vinita Jain <jayam6297@gmail.com>
2026-06-01 23:21:51 +0530

    Formated

--- src/app/observable/async-subject/async-subject.component.ts
+++ src/app/observable/async-subject/async-subject.component.ts
@@ -1,31 +1,27 @@
 import { Component, OnInit } from '@angular/core';
-import { interval, Subscription } from 'rxjs';
 import { AppService } from 'src/app/appService/app.service';
 
 @Component({
   selector: 'app-async-subject',
   templateUrl: './async-subject.component.html',
-  styleUrls: ['./async-subject.component.scss']
+  styleUrls: ['./async-subject.component.scss'],
 })
 export class AsyncSubjectComponent implements OnInit {
-
-  constructor(private _service: AppService) { }
+  constructor(private _service: AppService) {}
   asyncVideoEmit: any;
 
   ngOnInit(): void {
-    this._service.asyncVideoEmit.subscribe(res => {
+    this._service.asyncVideoEmit.subscribe((res) => {
       this.asyncVideoEmit = res;
-    })
+    });
   }
-  
+
   onVideoAdd(video) {
-    console.log(video);
-    this._service.asyncVideoEmit.next(video)
+    console.log('video', video);
+    this._service.asyncVideoEmit.next(video);
   }
-  
+
   onComplete() {
-    this._service.asyncVideoEmit.complete()
+    this._service.asyncVideoEmit.complete();
   }
-  
-  
 }
